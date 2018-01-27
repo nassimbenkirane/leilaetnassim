43e5fea9138a026f2cb8add212e1660076f6f5d5
Nassim Benkirane <nassim.benkirane@gmail.com>
2018-01-27 22:35:25 +0100

    Update liste-de-mariage-eloetthomas.vue

--- pages/liste-de-mariage-eloetthomas.vue
+++ pages/liste-de-mariage-eloetthomas.vue
@@ -1,9 +1,7 @@
-export default () => {
-  return (
-    <div>
-      <a target="_blank" href="https://www.ameliste.fr/list/list?site=thomasetelo" class="button is-primary">
-        Notre liste de mariage
-      </a>    
-    </div>
-  )
-}
+<template>
+  <div>
+    <a target="_blank" href="https://www.ameliste.fr/list/list?site=thomasetelo" class="button is-primary">
+      Notre liste de mariage
+    </a>    
+  </div>
+</template>
